c311d1f3787e8b53158fe92b870cda0332bc8a54
Saurav Sahu <sauravsahu.12@live.com>
2020-01-27 23:48:01 +0530

    use a hardlink to keep both original and repo updated

--- .customizations.sh
+++ .customizations.sh
@@ -1,1 +1,25 @@
-/Users/sauravsahu/.customizations.sh
+# activate antigen
+source /usr/local/share/antigen/antigen.zsh
+
+# use antigen to setup the terminal
+antigen use oh-my-zsh
+antigen bundle mafredri/zsh-async
+antigen bundle sindresorhus/pure
+
+# add zsh autosuggestions
+antigen bundle zsh-users/zsh-autosuggestions
+
+# apply all antigen changes
+antigen apply
+
+# nvm config
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# use current node LTS version with nvm
+# suppress output
+nvm use 12.14.1 &> /dev/null
+
+# alias cls to clear
+alias cls=clear
