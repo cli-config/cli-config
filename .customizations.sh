4a8cd91d4dabc1703b0f2c74d37909b869bf10b8
Saurav Sahu <mrsauravsahu@outlook.com>
2020-02-04 17:33:21 +0530

    update customizations.sh - alias yarn - setup thefuck - opt out of .NET Core Telemetry

--- .customizations.sh
+++ .customizations.sh
@@ -23,3 +23,15 @@ nvm use 12.14.1 &> /dev/null
 
 # alias cls to clear
 alias cls=clear
+
+# run ssh agent
+eval "$(ssh-agent -s)" &> /dev/null
+
+# alias y to yarn
+alias y=yarn
+
+# setup thefuck
+eval $(thefuck --alias)
+
+# opt out of .NET Core telemetry 
+export DOTNET_CLI_TELEMETRY_OPTOUT=true
