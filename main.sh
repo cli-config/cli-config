1f706e02057a0e57bdea897a300217646a2cce57
mrsauravsahu <mrsauravsahu@outlook.com>
2020-08-04 13:37:08 +0530

    install nvm with antigen

--- main.sh
+++ main.sh
@@ -5,17 +5,12 @@ source ~/config/src/antigen.zsh
 antigen use oh-my-zsh
 antigen bundle mafredri/zsh-async
 antigen bundle sindresorhus/pure
-
-# add zsh autosuggestions
+antigen bundle lukechilds/zsh-nvm
 antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-syntax-highlighting
 
 # apply all antigen changes
 antigen apply
-
-# nvm config
-export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # use current node LTS version with nvm
 # suppress output
--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 # Goto config dir
 cd ~/config
 
-
-
 # create src if not exists
 mkdir src || true
 cd src
@@ -15,8 +13,7 @@ curl -L git.io/antigen > antigen.zsh
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source ~/config/src/antigen.zsh
+antigen bundle lukechilds/zsh-nvm
+antigen apply
 nvm install --lts
