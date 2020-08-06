8e0f23c6ec029bf617834ff9a1842dea51873a7a
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-06 20:26:57 +0530

    make setup more resilient and install ohmyzsh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,39 @@
-#!/bin/bash
+#!/bin/zsh
 
+# Find the directory where setup.sh is
+CLI_CONFIG_ROOT=`dirname $0`
 # Goto config dir
-cd ~/config
+cd $CLI_CONFIG_ROOT
 
-# create src if not exists
-mkdir src || true
-cd src
+# Create src if not exists
+mkdir src > /dev/null 2> /dev/null || true
 
-# download antigen
-curl -L git.io/antigen > antigen.zsh
+# Download antigen into src folder
+curl -L git.io/antigen > ./src/antigen.zsh
 
-sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# TODO: move this to requirements
+# sudo apt install -y zsh
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 
-source ~/config/src/antigen.zsh
+# Load antigen
+. ./src/antigen.zsh
+antigen use oh-my-zsh
+
 antigen bundle lukechilds/zsh-nvm
 antigen apply
 nvm install --lts
+
+currentOs=`uname -s`
+if [ $currentOs = "Linux" ]; then
+    # echo 'You are on linux'
+    . ./config/linux.sh
+elif [ $currentOs = "Darwin" ]; then
+    # echo 'Mac huh'
+    brew install autojump direnv
+else
+    echo 'what realm is this?'
+fi
+
+echo '\n\nYou are ready to use cli-config now'
+echo "Paste this in your .zshrc \n\nCLI_CONFIG_ROOT='$CLI_CONFIG_ROOT'\nsource \"\$CLI_CONFIG_ROOT/main.sh\""
+echo "And restart your shell"
