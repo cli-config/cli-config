21a65a3117af38987d016188d749fa0a41c4e334
Saurav Sahu <mrsauravsahu@outlook.com>
2020-08-06 21:09:29 +0530

    Add info statements when setting up

--- setup.sh
+++ setup.sh
@@ -1,24 +1,32 @@
-#!/bin/zsh
+#! /usr/bin/env zsh
 
 # Find the directory where setup.sh is
+# This is relative path to the setup.sh script
 CLI_CONFIG_ROOT=`dirname $0`
+
 # Goto config dir
 cd $CLI_CONFIG_ROOT
+
+# Convert CLI_CONFIG_ROOT to absolute path
+CLI_CONFIG_ROOT=`pwd`
 
 # Create src if not exists
 mkdir src > /dev/null 2> /dev/null || true
 
 # Download antigen into src folder
+echo "\n\nCLI-CONFIG: Installing antigen plugin manager\n\n"
 curl -L git.io/antigen > ./src/antigen.zsh
 
 # TODO: move this to requirements
 # sudo apt install -y zsh
+echo "\n\nCLI-CONFIG: Installing ohmyzsh\n\n"
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 
 # Load antigen
 . ./src/antigen.zsh
 antigen use oh-my-zsh
 
+echo "\n\nCLI-CONFIG: Installing node.js with nvm\n\n"
 antigen bundle lukechilds/zsh-nvm
 antigen apply
 nvm install --lts
@@ -35,5 +43,6 @@ else
 fi
 
 echo '\n\nYou are ready to use cli-config now'
+echo '\n\nNote: Your default shell should be ''zsh''. You can change the default shell with chsh'
 echo "Paste this in your .zshrc \n\nCLI_CONFIG_ROOT='$CLI_CONFIG_ROOT'\nsource \"\$CLI_CONFIG_ROOT/main.sh\""
-echo "And restart your shell"
+echo "\n\nAnd restart your shell\n\n"
