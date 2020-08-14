596e28fc08f0ac6ad86d53e5b167a7720e26d9c6
Sahu, S <mrsauravsahu@outlook.com>
2020-08-14 18:34:25 +0530

    make PYENV_ROOT env relative (#16)

--- scripts.programs/pyenv.sh
+++ scripts.programs/pyenv.sh
@@ -13,6 +13,6 @@ fi
 
 
 echo "\n\n# pyenv configuration" >> $CLI_CONFIG_PROGRAMS_CONF
-echo "export PYENV_ROOT=\"$CLI_CONFIG_ROOT/programs/pyenv\"" >> $CLI_CONFIG_PROGRAMS_CONF
+echo 'export PYENV_ROOT="$CLI_CONFIG_ROOT/programs/pyenv"' >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"\n' >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'eval "$(pyenv init -)"' >> $CLI_CONFIG_PROGRAMS_CONF
