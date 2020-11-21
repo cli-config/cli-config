87e59d6241188beb7a64d3bfcece06f471abd9c4
Saurav Sahu <mrsauravsahu@outlook.com>
2020-11-21 23:41:41 +0530

    install current version of .NET (#29)

--- scripts/install.dotnet.sh
+++ scripts/install.dotnet.sh
@@ -1,6 +1,6 @@
 echo "CLI-CONFIG: Installing dotnet\n\n"
 
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --install-dir $CLI_CONFIG_ROOT/current/dotnet --channel LTS
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --install-dir $CLI_CONFIG_ROOT/current/dotnet --channel Current
 
 echo "\n\n# dotnet configuration" >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'export PATH="$CLI_CONFIG_ROOT/current/dotnet:$PATH"' >> $CLI_CONFIG_PROGRAMS_CONF
