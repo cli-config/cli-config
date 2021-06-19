4bd32196d8a69dd17b5e979c41c767061284dd71
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-19 18:59:14 +0530

    update nvm installer to be run through bash (#46)

--- scripts/install.nvm.sh
+++ scripts/install.nvm.sh
@@ -1,14 +1,15 @@
 echo "CLI-CONFIG: Installing nvm\n\n"
 
+echo "\n\nCLI-CONFIG: Installing node.js with nvm\n\n"
+zsh -c "
+. $CLI_CONFIG_ROOT/scripts/env.sh
 # Load antigen
 . $CLI_CONFIG_TOOLS_LOCATION/antigen.zsh
-antigen use oh-my-zsh
-
-echo "\n\nCLI-CONFIG: Installing node.js with nvm\n\n"
 NVM_DIR=$CLI_CONFIG_ROOT/current/nvm
 antigen bundle lukechilds/zsh-nvm
 antigen apply
 nvm install --lts
+"
 
 echo "\n\n# nvm configuration" >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'export NVM_DIR="$CLI_CONFIG_ROOT/current/nvm"' >> $CLI_CONFIG_PROGRAMS_CONF
