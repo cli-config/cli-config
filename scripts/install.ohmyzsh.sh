dcd53e1f1539c32ec2d26a0941ae19051f84f192
Saurav Sahu <mrsauravsahu@outlook.com>
2021-07-09 14:51:09 +0530

    keep original .zshrc file if already present (#52)

--- scripts/install.ohmyzsh.sh
+++ scripts/install.ohmyzsh.sh
@@ -1,7 +1,7 @@
 echo "CLI-CONFIG: Installing ohmyzsh\n\n"
 
 export ZSH="$CLI_CONFIG_ROOT/current/ohmyzsh"
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh -s -- --keep-zshrc
 
 echo "\n\n# ohmyzsh configuration" >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'export ZSH="$CLI_CONFIG_ROOT/current/ohmyzsh"' >> $CLI_CONFIG_PROGRAMS_CONF
