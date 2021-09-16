ab588511bbe2de5190519fc8cc1e6b530327d8b7
Saurav Sahu <mrsauravsahu@outlook.com>
2021-09-16 22:38:55 +0530

    fix: add pyenv shims directory to path (#57)

--- scripts/install.pyenv.sh
+++ scripts/install.pyenv.sh
@@ -20,6 +20,6 @@ git clone https://github.com/pyenv/pyenv-virtualenv.git $CLI_CONFIG_ROOT/current
 
 echo "\n\n# pyenv configuration" >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'export PYENV_ROOT="$CLI_CONFIG_ROOT/current/pyenv"' >> $CLI_CONFIG_PROGRAMS_CONF
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"\n' >> $CLI_CONFIG_PROGRAMS_CONF
+echo 'export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"\n' >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'eval "$(pyenv init -)"' >> $CLI_CONFIG_PROGRAMS_CONF
 echo 'eval "$(pyenv virtualenv-init -)"' >> $CLI_CONFIG_PROGRAMS_CONF
