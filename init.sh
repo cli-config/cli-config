34cc28b69d2c06306b917007d98e30cfbf5d71f2
Saurav Sahu <mrsauravsahu@outlook.com>
2021-09-25 11:40:07 +0530

    fix: issue with setup.sh not getting invoked from init.sh (#65)

--- init.sh
+++ init.sh
@@ -1,9 +1,5 @@
-CLI_CONFIG_ROOT=${1:cli-config}
-
 git clone https://github.com/mrsauravsahu/cli-config.git \
 --recurse-submodules \
 --depth 1
 
-cd $CLI_CONFIG_ROOT
-
-./setup.sh
+bash `pwd`/cli-config/setup.sh
