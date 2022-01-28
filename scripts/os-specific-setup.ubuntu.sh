edcc3a41b3735df256226db4f6d03340443cce72
Saurav Sahu <mrsauravsahu@outlook.com>
2022-01-28 18:50:04 +0530

    fix: remove python as dep (#85)

--- scripts/os-specific-setup.ubuntu.sh
+++ scripts/os-specific-setup.ubuntu.sh
@@ -5,11 +5,6 @@ cmd='sudo apt update --yes'
 Log "CLI-CONFIG: $cmd"
 eval $cmd
 
-Log "CLI-CONFIG: Setting up dependencies"
-cmd='sudo apt install --yes python python-dev python3-pip'
-Log "CLI-CONFIG: $cmd"
-eval $cmd
-
 packages='autojump thefuck'
 Log "CLI-CONFIG: Installing '$packages'"
 cmd="sudo apt install --yes $packages"
