5038e905499c61b95f1ae0faa59ec1d911ea3c96
Saurav Sahu <mrsauravsahu@outlook.com>
2022-05-21 18:36:22 +0530

    Improv: Install antigen bundles inside cli-config folder (#25)

--- src/installers/antigen.configure.sh
+++ src/installers/antigen.configure.sh
@@ -1,1 +1,23 @@
-# nothing to do here
+namespace cliConfig
+
+TOOL=antigen
+CONF=$CLI_CONFIG_CONF_LOCATION/$TOOL.conf.sh
+
+echo -n > $CONF
+tee $CONF > /dev/null <<EOF
+ADOTDIR=\${CLI_CONFIG_ROOT}/current/antigen
+source \${CLI_CONFIG_TOOLS_LOCATION}/antigen.zsh
+
+# use antigen to setup the terminal
+antigen bundle qoomon/zsh-lazyload
+antigen bundle zsh-users/zsh-autosuggestions
+# Doesn't work on Mac without direnv and autojump installed with brew
+# ptavares/zsh-direnv changed their default branch to main which broke antigen bundle - explicitly requesting main branch
+antigen bundle ptavares/zsh-direnv@main
+antigen bundle autojump
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle ohmyzsh/ohmyzsh plugins/git
+
+# apply all antigen changes
+antigen apply
+EOF
--- src/templates/programs.conf.sh
+++ src/templates/programs.conf.sh
@@ -1,23 +1,5 @@
-# activate antigen
-source $CLI_CONFIG_TOOLS_LOCATION/antigen.zsh
-
-# use antigen to setup the terminal
-antigen bundle qoomon/zsh-lazyload
-antigen bundle zsh-users/zsh-autosuggestions
-# Doesn't work on Mac without direnv and autojump installed with brew
-# ptavares/zsh-direnv changed their default branch to main which broke antigen bundle - explicitly requesting main branch
-antigen bundle ptavares/zsh-direnv@main
-antigen bundle autojump
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle ohmyzsh/ohmyzsh plugins/git
-
-# apply all antigen changes
-antigen apply
-
 # init all cli-config tools
-for tool in `ls -1 $CLI_CONFIG_CONF_LOCATION`; do
-  . $CLI_CONFIG_CONF_LOCATION/$tool
-done
+for tool in `ls -1 $CLI_CONFIG_CONF_LOCATION | sort`; do . $CLI_CONFIG_CONF_LOCATION/$tool; done
 
 # alias cls to clear
 alias cls=clear
