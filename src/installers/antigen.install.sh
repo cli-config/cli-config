2a7f9798b44bffcb442c0142a98928e39e23b1c4
Saurav Sahu <mrsauravsahu@outlook.com>
2022-05-21 18:30:24 +0530

    feat: configure antigen with its own configure script

--- src/installers/antigen.install.sh
+++ src/installers/antigen.install.sh
@@ -1,8 +1,9 @@
 namespace cliConfig
 
-if [[ -f "$CLI_CONFIG_ROOT/current/tools/antigen.zsh" ]]; then
+if [ -d "$CLI_CONFIG_ROOT/current/antigen" ]; then
     Log 'CLI-CONFIG: Seems cli-config/tools/antigen is already installed!'
 else
     Log "CLI-CONFIG: Installing antigen plugin manager"
     wget https://git.io/antigen -O $CLI_CONFIG_TOOLS_LOCATION/antigen.zsh
+    mkdir $CLI_CONFIG_ROOT/current/antigen 2> /dev/null
 fi
