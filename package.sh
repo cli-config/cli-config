7c80dfc3be88fb6f704df8c2d446fe8dac467906
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-15 23:11:03 +0530

    chore: add a test subcommand in package

--- package.sh
+++ package.sh
@@ -1,8 +1,32 @@
+#!/usr/bin/env bash
+
 package () {
   case $1 in
 
     "lint")
     docker run --rm -u "$(id -u):$(id -g)" -v "$PWD:/mnt" -w /mnt mvdan/shfmt:v3.5.1-alpine -i 2 -l -w src/**/*.zsh
+    ;;
+
+    "test")
+    for testFile in $(find __tests__ -regex '.*spec.zsh'); do
+      ANY_TESTS_FAILED=0
+      . ${testFile}
+
+      testsCount="${#tests[@]}"
+
+      for ((i=0; i<${testsCount}; i++)); do
+        # echo test $i "${tests[$i]}"
+        test_result=$(zsh -c "${tests[$i]}")
+
+        if [ "${test_result}" != "${results[$i]}" ]; then
+          echo "'${test_result}' != '${results[$i]}'"
+          ANY_TESTS_FAILED=1
+        fi
+      done
+    done
+
+    
+    exit ${ANY_TESTS_FAILED}
     ;;
 
     *)
