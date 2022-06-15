ee4f712db536bc63149419aa72d2080700b1177f
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-15 23:40:45 +0530

    fix: lint and add .sh files to lint

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@
 CI=true
 # Find the directory where setup.sh is
 # This is relative path to the setup.sh script
-CLI_CONFIG_ROOT=`dirname $0`
+CLI_CONFIG_ROOT=$(dirname $0)
 # Goto config dir
 cd $CLI_CONFIG_ROOT
 # Convert CLI_CONFIG_ROOT to absolute path
-CLI_CONFIG_ROOT=`pwd`
+CLI_CONFIG_ROOT=$(pwd)
 
-. $CLI_CONFIG_ROOT/src/defs/main.zsh 
-. $CLI_CONFIG_ROOT/src/utils/log.zsh 
+. $CLI_CONFIG_ROOT/src/defs/main.zsh
+. $CLI_CONFIG_ROOT/src/utils/log.zsh
 
 LOG_NAMESPACE='cli-config'
 
