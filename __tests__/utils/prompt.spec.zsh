7c81fbe41affda0c859bd1fceb292f15ef29c0f6
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-17 15:12:04 +0530

    todo: add todo tests for Prompt

---
+++ __tests__/utils/prompt.spec.zsh
@@ -0,0 +1,13 @@
+# TODO: use expect to simulate user input
+tests=()
+results=()
+
+# tests=(
+# '. src/utils/prompt.zsh; valid_opts=("y" "N"); Prompt "Are you sure?" "${valid_opts[@]}"'
+# '. src/utils/prompt.zsh; valid_opts=("yeah" "nah"); Prompt "You sure bruv" "${valid_opts[@]}"'
+# )
+# 
+# results=(
+# "Are you sure? (y/N) "
+# "You sure bruv (yeah/nah) "
+# )
