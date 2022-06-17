658ca30b4e359bb20a5ebadd802ebd285b18ddb3
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-17 14:28:50 +0530

    improv: update remove script to delete tool and its conf

--- src/defs/remove.zsh
+++ src/defs/remove.zsh
@@ -2,6 +2,14 @@
 
 remove() {
   for tool in "${CCOPT_TOOLS[@]}"; do
-    echo $tool
+    Log "Running uninstall script for '${tool}'..."
+    uninstallScriptPath="${CLI_CONFIG_ROOT}/src/installers/${tool}.uninstall.zsh" 
+    if [ -f "${uninstallScriptPath}" ]; then
+      . ${uninstallScriptPath}
+    else
+      rm -rf ${CLI_CONFIG_ROOT}/current/${tool} 2> /dev/null
+      rm -rf ${CLI_CONFIG_ROOT}/current/conf/${tool}.conf.sh
+    fi
+    
   done
 }
