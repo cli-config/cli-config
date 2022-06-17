98b75ae5cc8f20aeef993afd7c2855fdc3f5552c
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-17 14:47:43 +0530

    feat: prompt util with options validation

---
+++ src/utils/prompt.zsh
@@ -0,0 +1,16 @@
+. ${CLI_CONFIG_ROOT}/src/utils/array.zsh
+
+Prompt(){
+  message=$1
+  shift
+  valid_opts=("$@")
+
+  valid_opts_str=$(array_str "/" "${valid_opts[@]}")
+  echo -n "${message} (${valid_opts_str}) " > /dev/tty
+  read response
+
+  isValid=$(array_contains "${response}" "${valid_opts[@]}")
+
+  result=("${response}" "${isValid}")
+  echo ${result}
+}
