edcf85b2d361333a63214091c745b6a01e57bf45
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-21 01:47:23 +0530

    docs: usage literature updates

--- src/defs/usage.zsh
+++ src/defs/usage.zsh
@@ -5,13 +5,16 @@
 usage() {
   modes=('install' 'configure')
   profiles=($(ls -1 "${CLI_CONFIG_ROOT}/profiles"))
+  tools=$(ls -1 $CLI_CONFIG_ROOT/src/installers | sed 's/\..*$//g' | sort | uniq | xargs | sed 's/ /,/g')
 
-  printf "\nCLI-CONFIG \n\n"
+  printf "CLI-CONFIG\n"
 
   modes_str=$(array_str ", " "${modes[@]}")
-  echo -n "./setup.sh <${modes_str}> "
-
-  profiles_str=$(array_str ", " "${profiles[@]}")
-  printf "[-p|--profile] ${profiles_str} "
-  printf "[-t|--tools $(ls -1 $CLI_CONFIG_ROOT/src/installers | sed 's/\..*$//g' | sort | uniq | xargs | sed 's/ /,/g')]\n\n"
+  profiles_str=$(array_str "/" "${profiles[@]}")
+  
+  echo "./setup.sh <mode> [-p|--profile=profileName] [-t|--tools=tool1,tool2]"
+  printf "\n\n"
+  echo "mode: ${modes_str} "
+  echo "profile: ${profiles_str} "
+  printf "tools: ${tools}\n"
 }
