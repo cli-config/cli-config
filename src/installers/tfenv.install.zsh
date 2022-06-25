77d38d2fc65bbce7d0065add3ed50cb9c56a7554
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-25 17:40:20 +0530

    improv: install default version for tfenv

--- src/installers/tfenv.install.zsh
+++ src/installers/tfenv.install.zsh
@@ -1,3 +1,7 @@
+typeset -A options=(
+DEFAULT_VERSION '1.2.3'
+)
+
 if [ -d "$CLI_CONFIG_ROOT/current/tfenv" ]; then
   Log 'Seems cli-config/tfenv is already installed!'
 else
@@ -7,4 +11,6 @@ else
   CLI_CONFIG_TFENV_ROOT=$CLI_CONFIG_ROOT/current/tfenv
 
   git clone --depth=1 https://github.com/tfutils/tfenv.git $CLI_CONFIG_TFENV_ROOT
+  ${CLI_CONFIG_TFENV_ROOT}/bin/tfenv install ${options[DEFAULT_VERSION]}
+  ${CLI_CONFIG_TFENV_ROOT}/bin/tfenv use ${options[DEFAULT_VERSION]}
 fi
