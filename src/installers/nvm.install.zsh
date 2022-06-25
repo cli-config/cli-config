c38c967f380eada9fa38fa1f90e4cb9f51afa23a
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-25 17:49:14 +0530

    improv: install default node through options association array

--- src/installers/nvm.install.zsh
+++ src/installers/nvm.install.zsh
@@ -1,6 +1,11 @@
 TOOL=nvm
 CONF=$CLI_CONFIG_CONF_LOCATION/$TOOL.conf.sh
 
+typeset -A options=(
+  DEFAULT_VERSION '--lts'
+  EXTRA_LAZY_COMMANDS 'node npm npx'
+)
+
 if [ -d "$CLI_CONFIG_ROOT/current/nvm" ]; then
   Log 'Seems cli-config/nvm is already installed!'
 else
@@ -12,5 +17,5 @@ else
   wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | PROFILE=/dev/null NVM_DIR=$CLI_CONFIG_ROOT/current/nvm bash
 
   . $NVM_DIR/nvm.sh
-  nvm install --lts
+  nvm install ${options[DEFAULT_VERSION]}
 fi
