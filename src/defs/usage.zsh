f942cec7b67fb062295f95df22f6483bfc48c95a
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-26 01:25:51 +0530

    tmp: usage print extra options

--- src/defs/usage.zsh
+++ src/defs/usage.zsh
@@ -5,16 +5,27 @@
 usage() {
   modes=('install' 'configure')
   profiles=($(ls -1 "${CLI_CONFIG_ROOT}/profiles"))
-  tools=$(ls -1 $CLI_CONFIG_ROOT/src/installers | sed 's/\..*$//g' | sort | uniq | xargs | sed 's/ /,/g')
+  tools=($(ls -1 $CLI_CONFIG_ROOT/src/installers | sed 's/\..*$//g' | sort | uniq ))
 
   printf "CLI-CONFIG\n"
 
-  modes_str=$(array_str ", " "${modes[@]}")
+  modes_str=$(array_str "/" "${modes[@]}")
   profiles_str=$(array_str "/" "${profiles[@]}")
+  tools_str=$(array_str "," "${tools[@]}")
 
   echo "./setup.sh <mode> [-p|--profile=profileName] [-t|--tools=tool1,tool2]"
   printf "\n\n"
   echo "mode: ${modes_str} "
   echo "profile: ${profiles_str} "
-  printf "tools: ${tools}\n"
+  printf "tools: ${tools_str}\n"
+
+  for tool in "${tools[@]}"; do
+    . "${CLI_CONFIG_ROOT}/src/installers/${tool}.zsh"
+    if [ ! "${#options[@]}" = "0" ]; then
+      echo "  ${tool}"
+      for key in "${(@k)options}"; do
+        echo "    --$(echo $key | sed -e 's/_/-/g' | tr 'A-Z' '[:lower:]' )"
+      done
+    fi
+  done
 }
