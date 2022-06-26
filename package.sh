f90209e1af1a3f1fe3bf8d57b9282a07bd40e178
Saurav Sahu <mrsauravsahu@outlook.com>
2022-06-26 15:51:27 +0530

    chore: lint cli-config as well

--- package.sh
+++ package.sh
@@ -4,7 +4,7 @@ package() {
   case $1 in
 
   "lint")
-    docker run --rm -u "$(id -u):$(id -g)" -v "$PWD:/mnt" -w /mnt mvdan/shfmt:v3.5.1-alpine -i 2 -l -w src/**/*.zsh *.sh
+    docker run --rm -u "$(id -u):$(id -g)" -v "$PWD:/mnt" -w /mnt mvdan/shfmt:v3.5.1-alpine -i 2 -l -w src/**/*.zsh *.sh cli-config
     ;;
 
   "test")
