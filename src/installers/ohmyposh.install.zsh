3959d9f82d065f38988ff83ff8e219f0d6d1310c
Saurav Sahu <mrsauravsahu@outlook.com>
2025-01-09 01:02:46 +0530

    Feat: Pin oh-my-posh to v24.18.0 (#76)
    feat: pin oh-my-posh to v24.18.0

--- src/installers/ohmyposh.install.zsh
+++ src/installers/ohmyposh.install.zsh
@@ -2,12 +2,14 @@ TOOL=ohmyposh
 CONF=$CLI_CONFIG_CONF_LOCATION/$TOOL.conf.sh
 INSTALL_DIR=$CLI_CONFIG_ROOT/current/ohmyposh
 
+APP_VERSION='v24.18.0'
+
 if [ -d $INSTALL_DIR ]; then
   Log 'Seems cli-config/ohmyposh is already installed!'
 else
   Log "Installing ohmyposh"
   mkdir $INSTALL_DIR
-  wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-$(uname -s | tr 'A-Z' 'a-z')-amd64 -O $INSTALL_DIR/oh-my-posh
+  wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/${APP_VERSION}/posh-$(uname -s | tr 'A-Z' 'a-z')-amd64 -O $INSTALL_DIR/oh-my-posh
   chmod +x $INSTALL_DIR/oh-my-posh
 
   Log "Setting up ohmyposh themes"
